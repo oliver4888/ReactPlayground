6bf5aa0fa326033ec4ff1417e78c5b5ae797653c
Oliver <8377772+oliver4888@users.noreply.github.com>
2019-09-02 00:00:53 +0100

    Add useState hook to Counter component.

--- ReactTemplate/ClientApp/src/components/pages/Counter.js
+++ ReactTemplate/ClientApp/src/components/pages/Counter.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { observer } from "mobx-react";
 import { Button } from "react-bootstrap";
 
 import CounterStore from "../stores/CounterStore";
 
 const Counter = observer(function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Counter</h1>
@@ -12,10 +14,19 @@ const Counter = observer(function Counter() {
       <p>This is a simple example of a React component.</p>
 
       <p>
-        Current count: <strong>{CounterStore.count}</strong>
+        Session count: <strong>{CounterStore.count}</strong>
+      </p>
+      <p>
+        Current count: <strong>{count}</strong>
       </p>
 
-      <Button variant="primary" onClick={CounterStore.incrementCounter}>
+      <Button
+        variant="primary"
+        onClick={() => {
+          CounterStore.incrementCounter();
+          setCount(count + 1);
+        }}
+      >
         Increment
       </Button>
     </div>

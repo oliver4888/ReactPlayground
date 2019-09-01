6c40bc968d02510d71efb665925ef0ec50b5a124
Oliver <8377772+oliver4888@users.noreply.github.com>
2019-09-01 15:43:59 +0100

    Replace reactstrap with react-bootstrap.

--- ReactTemplate/ClientApp/src/components/NavMenu.js
+++ ReactTemplate/ClientApp/src/components/NavMenu.js
@@ -1,46 +1,36 @@
-import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import React, { Component } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./NavMenu.css";
 
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
-
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
+  render() {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Navbar
+          bg="light"
+          expand="lg"
+          className="bg-white border-bottom box-shadow mb-3"
+          collapseOnSelect
+        >
           <Container>
-            <NavbarBrand tag={Link} to="/">ReactTemplate</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
+            <LinkContainer to="/">
+              <Navbar.Brand>ReactTemplate</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse>
+              <Nav className="mr-auto">
+                <LinkContainer className="text-dark" to="/">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer className="text-dark" to="/counter">
+                  <Nav.Link>Counter</Nav.Link>
+                </LinkContainer>
+                <LinkContainer className="text-dark" to="/fetch-data">
+                  <Nav.Link>Fetch data</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
       </header>
